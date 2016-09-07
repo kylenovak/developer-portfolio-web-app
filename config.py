e9e49c522143d4ac54b05752e42633124653d74f
Kyle Novak <kylenovak@github.com>
2016-09-07 01:37:38 -0500

    keygen and config

---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+import keygen
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = keygen.get_random_key()
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
