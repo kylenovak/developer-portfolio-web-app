e1d2344d662ea92a93a62e72a20923fbbf681356
Kyle Novak <kylenovak@github.com>
2016-10-29 16:20:29 -0500

    fix renaming

--- kylejnovak/services/before_requests.py
+++ kylejnovak/services/before_requests.py
@@ -1,10 +1,10 @@
 from flask import g, Markup
 
-from .. import app
+from .. import application
 from . import copyright
 
 
-@app.before_request
+@application.before_request
 def before_requests():
     # mark this text as safe
     g.copyright_text = Markup(copyright.get_copyright_text(app))
--- kylejnovak/services/error_handlers.py
+++ kylejnovak/services/error_handlers.py
@@ -1,7 +1,7 @@
 from flask import render_template
-from .. import app
+from .. import application
 
 
-@app.errorhandler(404)
+@application.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
