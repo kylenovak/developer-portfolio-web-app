ecaf3ada0818464381eefd77a1e716cd20b7d80b
Kyle Novak <kylenovak@github.com>
2016-10-30 02:03:50 -0500

    login logging

--- kylejnovak/views/login/login.py
+++ kylejnovak/views/login/login.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, flash, url_for
+from flask import Blueprint, render_template, redirect, flash, url_for, request
 from flask_login import login_user, login_required, logout_user
 
 from kylejnovak.forms.login import LoginForm
@@ -10,13 +10,20 @@ login_page = Blueprint('login_page', __name__, template_folder='templates')
 @login_page.route('/login', methods=('GET', 'POST'))
 def login():
     login_form = LoginForm()
-    app.logger.info("Attempting to login user:".format(login_form.username))
+
+    if request.method == 'POST':
+        app.logger.info("Attempting to login as user: {}".format(login_form.username))
+
     if login_form.validate_on_submit():
         # Tell Flask-Login that user has been authenticated.
         login_user(login_form.user)
-        app.logger.info("Logged in as user:".format(login_form.user))
+
         flash('User logged in successfully.')
+        app.logger.info("Logged in as user: {}".format(login_form.user))
+
         return redirect(url_for('admin.index'))
+
+    # Login failed, show login template.
     return render_template('login.html', login_form=login_form)
 
 
