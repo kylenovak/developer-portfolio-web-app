6a6d17b526f5be357bff55481b60b8f222f08698
Kyle Novak <kylenovak@github.com>
2016-12-18 19:41:34 -0600

    internal error handling

--- kylejnovak/services/error_handlers.py
+++ kylejnovak/services/error_handlers.py
@@ -3,5 +3,12 @@ from kylejnovak import app
 
 
 @app.errorhandler(404)
-def page_not_found(error):
+def page_not_found(exception):
+    app.logger.error(exception)
     return render_template('page_not_found.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(exception):
+    app.logger.error(exception)
+    return render_template('internal_error.html'), 500
