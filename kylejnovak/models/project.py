8146ee0648cdd14311393d1dc557b84533b25c8d
Kyle Novak <kylenovak@github.com>
2016-12-31 02:46:25 -0600

    refactor

--- kylejnovak/models/project.py
+++ kylejnovak/models/project.py
@@ -9,6 +9,7 @@ class Project(db.Model):
     title = db.Column(db.String(255), nullable=False, unique=True)
     url_slug = db.Column(db.String(255), nullable=False, unique=True)
     content = db.Column(db.Text, nullable=False)
+    is_complete = db.Column(db.String(1), nullable=False, default='n', server_default='n')
     create_date = db.Column(db.DateTime, default=datetime.utcnow(), nullable=False)
     update_date = db.Column(db.DateTime, default=datetime.utcnow(), nullable=False)
 
